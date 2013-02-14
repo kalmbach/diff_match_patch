af0568fa2c3302d46e18babd53f3e9535d612d54
kalmbach <kalmbach@gmail.com>
2013-02-14 01:58:58 -0300

    starting to move diff_ methods to their own module

--- lib/diff_match_patch.rb
+++ lib/diff_match_patch.rb
@@ -1,9 +1,11 @@
+require 'diff_methods'
 require 'patch_obj'
 
 # Class containing the diff, match and patch methods.
 # Also contains the behaviour settings.
 class DiffMatchPatch
-  attr_accessor :diff_timeout
+  include DiffMethods
+
   attr_accessor :diff_editCost
   attr_accessor :match_threshold
   attr_accessor :match_distance
@@ -11,14 +13,10 @@ class DiffMatchPatch
   attr_accessor :patch_margin
   attr_reader :match_maxBits
 
-  FIXNUM_MAX =  2**(0.size * 8 - 2) - 1
-
   def initialize
     # Inits a diff_match_patch object with default settings.
     # Redefine these in your program to override the defaults.
 
-    # Number of seconds to map a diff before giving up (0 for infinity).
-    @diff_timeout = 1
     # Cost of an empty edit operation in terms of edit characters.
     @diff_editCost = 4
     # At what point is no match declared (0.0 = perfection, 1.0 = very loose).
@@ -40,110 +38,7 @@ class DiffMatchPatch
     # However to avoid long patches in certain pathological cases, use 32.
     # Multiple short patches (using native ints) are much faster than long ones.
     @match_maxBits = 32
-  end
-
-
-  # Find the differences between two texts.  Simplifies the problem by 
-  # stripping any common prefix or suffix off the texts before diffing.
-  def diff_main(text1, text2, checklines=true, deadline=nil)
-    # Set a deadline by which time the diff must be complete.
-    deadline ||= Time.now + (diff_timeout.zero? ? FIXNUM_MAX : diff_timeout)
-
-    # Check for null inputs.
-    raise ArgumentError.new('Null inputs. (diff_main)') unless text1 || text2
-
-    # Check for equality (speedup).
-    return (text1.empty? ? [] : [[:equal, text1]]) if text1 == text2
-
-    # Trim off common prefix and suffix(speedup).
-    common_prefix, text1, text2 = diff_trimCommonPrefix(text1, text2)
-    common_suffix, text1, text2 = diff_trimCommonSuffix(text1, text2)
-
-    # Compute the diff on the middle block.
-    diffs = diff_compute(text1, text2, checklines, deadline)
-
-    # Restore the prefix and suffix.
-    diffs.unshift([:equal, common_prefix]) unless common_prefix.nil?
-    diffs.push([:equal, common_suffix]) unless common_suffix.nil?
-    diff_cleanupMerge(diffs)
-
-    diffs
-  end
-
-  def diff_trimCommonPrefix(text1, text2)
-    if (common_length = diff_commonPrefix(text1, text2)).nonzero?
-      common_prefix = text1[0...common_length]
-      text1 = text1[common_length..-1]
-      text2 = text2[common_length..-1]
-    end
-
-    return [common_prefix, text1, text2]
-  end
-
-  private :diff_trimCommonPrefix
-
-  def diff_trimCommonSuffix(text1, text2)
-    if (common_length = diff_commonSuffix(text1, text2)).nonzero?
-      common_suffix = text1[-common_length..-1]
-      text1 = text1[0...-common_length]
-      text2 = text2[0...-common_length]
-    end
-
-    return [common_suffix, text1, text2]
-  end
-
-  private :diff_trimCommonSuffix
-
-  # Find the differences between two texts.  Assumes that the texts do not
-  # have any common prefix or suffix.
-  def diff_compute(text1, text2, checklines, deadline)
-    # Just add some text (speedup).
-    return [[:insert, text2]] if text1.empty?
-  
-    # Just delete some text (speedup).
-    return [[:delete, text1]] if text2.empty?
-  
-    shorttext, longtext = [text1, text2].sort_by(&:length)
-    if i = longtext.index(shorttext)
-      # Shorter text is inside the longer text (speedup).
-      diffs = [[:insert, longtext[0...i]], [:equal, shorttext],
-               [:insert, longtext[(i + shorttext.length)..-1]]]
-
-      # Swap insertions for deletions if diff is reversed.
-      if text1.length > text2.length
-          diffs[0][0] = :delete
-          diffs[2][0] = :delete
-      end
-
-      return diffs
-    end
-
-    if shorttext.length == 1
-      # Single character string.
-      # After the previous speedup, the character can't be an equality.
-      return [[:delete, text1], [:insert, text2]]
-    end
-  
-    # Garbage collect.
-    longtext = nil 
-    shorttext = nil
-  
-    # Check to see if the problem can be split in two.
-    if hm = diff_halfMatch(text1, text2)
-      # A half-match was found, sort out the return data.
-      text1_a, text1_b, text2_a, text2_b, mid_common = hm
-      # Send both pairs off for separate processing.
-      diffs_a = diff_main(text1_a, text2_a, checklines, deadline)
-      diffs_b = diff_main(text1_b, text2_b, checklines, deadline)
-      # Merge the results.
-      return diffs_a + [[:equal, mid_common]] + diffs_b
-    end
-  
-    if checklines && text1.length > 100 && text2.length > 100
-      return diff_lineMode(text1, text2, deadline)
-    end
-  
-    return diff_bisect(text1, text2, deadline)
+    super
   end
 
   # Do a quick line-level diff on both strings, then rediff the parts for
